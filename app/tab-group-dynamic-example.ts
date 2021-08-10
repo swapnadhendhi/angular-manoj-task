68b7f222e727893f8955996c84b301775d818f95
swapnadhendhi <swapna.dhendhi@gmail.com>
2021-08-10 23:03:21 +0530

    2 files modified

--- app/tab-group-dynamic-example.ts
+++ app/tab-group-dynamic-example.ts
@@ -1,5 +1,5 @@
-import {Component} from '@angular/core';
-import {FormControl} from '@angular/forms';
+import { Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
 
 /**
  * @title Tab group with dynamically changing tabs
@@ -7,12 +7,25 @@ import {FormControl} from '@angular/forms';
 @Component({
   selector: 'tab-group-dynamic-example',
   templateUrl: 'tab-group-dynamic-example.html',
-  styleUrls: ['tab-group-dynamic-example.css'],
+  styleUrls: ['tab-group-dynamic-example.css']
 })
 export class TabGroupDynamicExample {
   tabs = ['First', 'Second', 'Third'];
   selected = new FormControl(0);
-
+  months = [
+    'Jan',
+    'Feb',
+    'Mar',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December'
+  ];
   addTab(selectAfterAdding: boolean) {
     this.tabs.push('New');
 
@@ -25,8 +38,3 @@ export class TabGroupDynamicExample {
     this.tabs.splice(index, 1);
   }
 }
-
-
-/**  Copyright 2018 Google Inc. All Rights Reserved.
-    Use of this source code is governed by an MIT-style license that
-    can be found in the LICENSE file at http://angular.io/license */
